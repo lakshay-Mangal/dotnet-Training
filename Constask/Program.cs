728d608f4c9b119d55991d4b87c0a75ee7557ae8
lakshay <lakshaymangal51@gmail.com>
2026-02-18 10:03:45 +0530

    More small programs and codes

---
+++ Constask/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+
+class Product
+{   
+     public string Name
+        {
+            get; 
+            set;
+        }
+        public int Id
+        {
+            get;
+            set;
+        }
+        public int Price
+        {
+            get;
+            set;
+        }
+        public int Quant
+        {
+            get;
+            set;
+        }
+        public bool Iso { get; set;}
+
+        public DateTime Date{get; set;}
+
+    public Product(String Name , int Id , int Price, int Quant , DateTime Date, bool Iso)
+    {
+        this.Name =Name;
+        this.Id=Id;
+        this.Price=Price;
+        this.Quant=Quant;
+        this.Date=Date;
+        this.Iso= Iso;
+    }
+            public void GetProductData()
+        {
+            Console.WriteLine("Enter Product name:");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter Product id:");
+            Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Product Price:");
+            Price= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Product Quantity:");
+            Quant = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Product ISO Standard Check:");
+            Iso = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Product Expiry Date:");
+            Date = DateTime.ParseExact(Console.ReadLine(), "yyyy-MM-dd",null);
+        }
+        public void DisplayProductData()
+        {
+            Console.WriteLine($"Product Name: {Name}");
+            Console.WriteLine($"Product Id: {Id}");
+            Console.WriteLine($"Product Price: {Price}");
+            Console.WriteLine($"Product Quantity: {Quant}");
+            Console.WriteLine($"Product Iso standard check: {Iso}");
+            Console.WriteLine($"Product expiry Date: {Date}");
+        }
+
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+
+        Product p1 = new Product("Laptop", 1, 50000, 2, DateTime.Now, true);
+        p1.DisplayProductData();
+    
+    }
+}
---
+++ staticConst/Program.cs
@@ -0,0 +1,59 @@
+﻿// using System;
+
+// class ODLexercise
+// {
+//     private static int number;
+//     public static int Number
+//     {
+//         get
+//         {
+//             return number;
+//         }
+//     }
+//     static ODLexercise()
+//     {
+//         Random r = new Random();
+//         number = r.Next();
+//     }
+// }
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Static Number ="+ ODLexercise.Number);
+//     }
+// }
+class ODLexercise
+{
+    private int number;
+
+    public int Number
+    {
+        get { return number; }
+    }
+
+    public ODLexercise()
+    {
+        Random r = new Random();
+        number = r.Next();
+    }
+    public ODLexercise(int seed)
+    {
+        Random r = new Random(seed);
+        number = r.Next();
+    }
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        ODLexercise num = new ODLexercise();
+        Console.WriteLine("Number=" + num.Number);
+
+        ODLexercise num1 = new ODLexercise(400);
+                Console.WriteLine("Number=" + num1.Number);
+
+    }
+}
